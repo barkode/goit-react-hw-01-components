c64a7f2e1f4407ffc145fbc689f81011ea697d5f
barkode <konstantinks@gmail.com>
2023-05-20 16:16:33 +0100

    add transactions

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,3 +1,22 @@
-function TransactionHistory() {}
+import PropTypes from 'prop-types';
 
-export default TransactionHistory;
+import { TransactionHistoryItem } from './TransactionHistoryItem';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>{items.map(item => TransactionHistoryItem(item))}</tbody>
+    </table>
+  );
+};
+
+TransactionHistoryItem.propTypes = {
+  items: PropTypes.array,
+};
