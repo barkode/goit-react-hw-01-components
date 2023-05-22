3599980b62ce6ccd0673669ef1a279514719fa79
barkode <konstantinks@gmail.com>
2023-05-23 00:36:31 +0100

    friends

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
 import { FriendListItem } from 'components/FriendList/FriendListItem';
+import css from './FriendList.module.css';
 
 export const FriendList = ({ friends }) => {
   return (
-    <ul className="friend-list">
+    <ul className={css['friend-list']}>
       {friends.map(friend => FriendListItem(friend))}
     </ul>
   );
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,12 +1,21 @@
 import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
 
 export const FriendListItem = friend => {
   const { avatar, name, isOnline, id } = friend;
   return (
-    <li className="item" key={id}>
-      <span className="status">{isOnline}</span>
-      <img className="avatar" src={avatar} alt="User avatar" width="48" />
-      <p className="name">{name}</p>
+    <li className={css.item} key={id}>
+      <span
+        className={
+          isOnline
+            ? `${css.status} ${css.online}`
+            : `${css.status} ${css.offline}`
+        }
+      >
+        {isOnline}
+      </span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
     </li>
   );
 };
