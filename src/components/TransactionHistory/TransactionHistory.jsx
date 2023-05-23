9e74e036b69d64d5a2ba599dff39f0c3134083bb
barkode <konstantinks@gmail.com>
2023-05-23 01:00:41 +0100

    transaction

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,18 +1,21 @@
 import PropTypes from 'prop-types';
 import { TransactionHistoryItem } from './TransactionHistoryItem';
+import css from './TransactionHistory.module.css';
 
 export const TransactionHistory = ({ items }) => {
   return (
-    <table className="transaction-history">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-      <tbody>{items.map(item => TransactionHistoryItem(item))}</tbody>
-    </table>
+    <div className={css.wrapper}>
+      <table className={css['transaction-history']}>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+        <tbody>{items.map(item => TransactionHistoryItem(item))}</tbody>
+      </table>
+    </div>
   );
 };
 
